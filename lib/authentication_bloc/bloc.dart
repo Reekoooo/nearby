3061222bffa9680dbe57af4a2a940935fb9b3723
saed <sa3eednabil@gmail.com>
2019-06-01 10:33:36 +0200

    added authentication bloc

---
+++ lib/authentication_bloc/bloc.dart
@@ -0,0 +1,3 @@
+export 'package:nearby/authentication_bloc/authentication_bloc.dart';
+export 'package:nearby/authentication_bloc/authentication_event.dart';
+export 'package:nearby/authentication_bloc/authentication_state.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:nearby/repositories/user_repository.dart';
 import 'package:nearby/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nearby/screens/splash_screen.dart';
+import 'package:nearby/simple_bloc_delegate.dart';
 
-void main() => runApp(MyApp());
+import 'authentication_bloc/authentication_bloc.dart';
+import 'authentication_bloc/authentication_event.dart';
+import 'authentication_bloc/authentication_state.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final UserRepository _userRepository = UserRepository();
+  AuthenticationBloc _authenticationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
+    _authenticationBloc.dispatch(AppStarted());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Near By',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      bloc: _authenticationBloc,
+      child: MaterialApp(
+        title: 'Near By',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BlocBuilder(
+          bloc: _authenticationBloc,
+          builder: (BuildContext context, AuthenticationState state) {
+            if (state is Uninitialized) {
+              return SplashScreen();
+            }
+            if (state is Authenticated) {
+              return HomeScreen(name: state.displayName);
+            }
+          }
+
+        ),
       ),
-      home: HomeScreen(),
     );
+  }
+
+  @override
+  void dispose() {
+    _authenticationBloc.dispose();
+    super.dispose();
   }
 }
